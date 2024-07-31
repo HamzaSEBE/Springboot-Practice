cddf7a7aefe941c80cc12c2b00d8e566ed7a5475
HamzaSEBE <163665205+HamzaSEBE@users.noreply.github.com>
2024-07-31 12:47:28 +0300

    Add files via upload

---
+++ AnagramResult.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class AnagramResult {
+    private List<String> words;
+    private Boolean isAnagram;
+
+
+    public AnagramResult() {}
+
+    public AnagramResult(List<String> words, Boolean isAnagram) {
+        this.words = words;
+        this.isAnagram = isAnagram;
+    }
+
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public void setWords(List<String> words) {
+        this.words = words;
+    }
+
+    public Boolean getIsAnagram() {
+        return isAnagram;
+    }
+
+    public void setIsAnagram(Boolean isAnagram) {
+        this.isAnagram = isAnagram;
+    }
+}
---
+++ JsonAnagram.java
@@ -0,0 +1,63 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class JsonAnagram {
+
+
+    public void read() throws FileNotFoundException, IOException {
+        File f = new File("src/data.txt");
+        Scanner scan = new Scanner(f);
+
+
+        List<AnagramResult> results = new ArrayList<>();
+
+        while (scan.hasNext()) {
+            String anagram1 = scan.next().toLowerCase();
+            String anagram2 = scan.next().toLowerCase();
+
+
+            Map<Character, Integer> s1 = createFrequencyMap(anagram1);
+            Map<Character, Integer> s2 = createFrequencyMap(anagram2);
+
+
+            Boolean isAnagram = s1.equals(s2);
+
+
+            results.add(new AnagramResult(List.of(anagram1, anagram2), isAnagram));
+
+            // Print result to console (optional)
+            if (isAnagram) {
+                System.out.println(anagram1 + " " + anagram2 + " ====== this is Anagram ====== \n");
+            } else {
+                System.out.println(anagram1 + " " + anagram2 + " ====== this is Not Anagram ====== \n");
+            }
+        }
+        scan.close();
+
+
+        writeResultsToJson(results, "IsAnagram.json");
+    }
+// شفتها بفيديو ونا بتعلم json فهمتها وطبقتها
+    public Map<Character, Integer> createFrequencyMap(String word) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        return frequencyMap;
+    }
+
+   public void writeResultsToJson(List<AnagramResult> results, String isAnagram) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(isAnagram), results);
+    }
+
+
+    
+}
